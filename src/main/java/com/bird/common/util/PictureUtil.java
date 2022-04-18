685cc16f4e3f40664f21899f6411be9f244385ad
birdBrother <jsaltar@163.com>
2022-04-18 13:49:05 +0800

    update 0.4.5

--- src/main/java/com/bird/common/util/PictureUtil.java
+++ src/main/java/com/bird/common/util/PictureUtil.java
@@ -1,8 +1,11 @@
 package com.bird.common.util;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -46,7 +49,7 @@ public class PictureUtil {
      **/
     public static Color getRandomColor() {
         String[] beautifulColors =
-                new String[]{"0,179,54", "1,83,255","168,57,208","208,57,173","92,57,208"};
+                new String[]{"52, 145, 250", "20, 201, 201","90, 170, 251","63, 212, 207"};
         int len = beautifulColors.length;
         Random random = new Random();
         String[] color = beautifulColors[random.nextInt(len)].split(",");
@@ -54,22 +57,82 @@ public class PictureUtil {
                 Integer.parseInt(color[2]));
     }
 
-    /**
-     * 是否是中文
-     * @author:bird
-     * @date: 2021-7-21 11:39
-     * @param: String str
-     * @return: boolean
-     **/
-    public static boolean isChinese(String str) {
-        String regEx = "[\\u4e00-\\u9fa5]+";
-        Pattern p = Pattern.compile(regEx);
-        Matcher m = p.matcher(str);
-        if (m.find()) {
-            return true;
-        }
-        else {
-            return false;
-        }
+
+    public static String fileType(String fileName){
+        return fileName.contains(".") ? fileName.substring(fileName.lastIndexOf("."), fileName.length()) : null;
     }
+
+    /**
+     * 头像
+     * @author: bird
+     * @date: 2022-4-18 13:43
+     * @param:
+     * @return:
+     **/
+    public static String headIcon(String name,String outputPath, String outputName, String baseDir, String baseUrl)
+            throws IOException {
+
+        int width = 200;
+        int height = 200;
+        int nameLen = name.length();
+        String first = name.substring(0, 1);
+        String nameWritten;
+        //如果用户输入的姓名少于等于3个字符，不用截取
+        if (nameLen <= 3 && ToolsUtil.isChinese(first)) {
+            nameWritten = name;
+        } else {
+            nameWritten = name.substring(0,1);
+        }
+        //文件存储路径
+        String pathName = outputPath + File.separator + outputName + ".png";
+        String filename = baseDir + pathName;
+        File file = new File(filename);
+
+        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = (Graphics2D) bi.getGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.setBackground(PictureUtil.getRandomColor());
+        g2.clearRect(0, 0, width, height);
+        g2.setPaint(Color.WHITE);
+        Font font = null;
+        switch (nameWritten.length()){
+            case 3:
+                font = new Font("微软雅黑", Font.PLAIN, 60);
+                g2.setFont(font);
+                g2.drawString(nameWritten, 14, 120);
+                break;
+            case 2:
+                font = new Font("微软雅黑", Font.PLAIN, 90);
+                g2.setFont(font);
+                g2.drawString(nameWritten, 12, 130);
+                break;
+            case 1:
+                //中文
+                if(ToolsUtil.isChinese(nameWritten)) {
+                    font = new Font("微软雅黑", Font.PLAIN, 130);
+                    g2.setFont(font);
+                    g2.drawString(nameWritten, 35, 140);
+                }
+                //英文
+                else {
+                    font = new Font("微软雅黑", Font.PLAIN, 140);
+                    g2.setFont(font);
+                    g2.drawString(nameWritten.toUpperCase(), 50, 140);
+                }
+                break;
+            default:
+                font = new Font("微软雅黑", Font.PLAIN, 130);
+                g2.setFont(font);
+                g2.drawString(nameWritten, 35, 140);
+                break;
+        }
+
+        BufferedImage rounded = PictureUtil.makeRoundedCorner(bi, 99);
+        ImageIO.write(rounded, "png", file);
+        return  pathName;
+    }
+
+
+
 }
