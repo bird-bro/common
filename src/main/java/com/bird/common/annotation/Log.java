4397282c4248cad19f6c67ae1a3788959ecc1936
birdbro <272898742@qq.com>
2022-06-10 14:09:34 +0800

    update 1.0.3

--- src/main/java/com/bird/common/annotation/Log.java
+++ src/main/java/com/bird/common/annotation/Log.java
@@ -3,8 +3,9 @@ package com.bird.common.annotation;
 import java.lang.annotation.*;
 
 /**
- * @author bird
- * @date 2022-4-7 13:31
+ * 日志注解
+ * @author birdbro
+ * @date 13:58 2022-6-10
  **/
 @Target(ElementType.METHOD) //注解放置的目标位置，METHOD是可注解在方法级别上
 @Retention(RetentionPolicy.RUNTIME) //注解在哪个阶段执行
@@ -13,6 +14,5 @@ public @interface Log {
     String value() default "";   // 操作模块
     String type() default "";   // 操作类型
     String desc() default "";  // 操作说明
-    boolean record() default false;
-    boolean persistent() default true;  // 是否持久化日志
+
 }
--- src/main/java/com/bird/common/aspect/LogHandle.java
+++ src/main/java/com/bird/common/aspect/LogHandle.java
@@ -1,18 +1,10 @@
 package com.bird.common.aspect;
 
 /**
- * @author bird
- * @date 2022-4-7 13:32
+ * @author birdbro
+ * @date 14:05 2022-6-10
  **/
 public interface LogHandle<T> {
-
-    /**
-     * 处理日志
-     * @param t 日志对象
-     * @param isPersistent 是否需要持久化 true表示需要持久化、false表示不需要
-     * @throws Exception
-     */
-    void processLog(T t,boolean isPersistent) throws Exception;
 
     /**
      * 持久化日志
@@ -21,11 +13,5 @@ public interface LogHandle<T> {
      */
     void persistenceLog(T t) throws Exception;
 
-    /**
-     * 是否需要持久化日志
-     * @param isPersistent true表示需要持久化日志
-     * @return boolean true表示持久化日志，false表示不持久化日志
-     */
-    boolean ifPersistenceLog(boolean isPersistent);
 
 }
